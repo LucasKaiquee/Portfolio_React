58012688eca9e80eedf5088aee1111a8dd3374f0
Lucas Kaique <lucaskaique743@gmail.com>
2023-07-14 16:03:19 -0300

    Feat: adicionando o componente SectionTitle

---
+++ src/components/SectionTitle.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line react/prop-types
+const SectionTitle = ({title}) => {
+    return(
+        <h2 className="ml-[50px] my-[50px] text-5xl font-bold">{title}<span className="text-[#00B3FF]">.</span></h2>
+    )
+}
+
+export default SectionTitle
