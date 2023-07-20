3258615ad70235ea1447714e4ccf39ce49749a18
Lucas Kaique <lucaskaique743@gmail.com>
2023-07-20 11:58:50 -0300

    Feat: criando o footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import About from './components/About'
 import SkillSection from './components/SkillSection'
 import ProjectSection from './components/ProjectSection'
+import ContactSection from './components/ContactSection'
 
 function App() {
   return (
@@ -12,6 +13,12 @@ function App() {
       <About />
       <SkillSection />
       <ProjectSection />
+      <ContactSection />
+
+      <footer className="relative bottom-0 w-[100%] bg-[#00B3FF] text-[#000] font-bold flex justify-evenly items-center py-5 text-xl">
+          <p>Portfólio Pessoal</p>
+          <p>Copyright © 2023 Lucas Kaique. <br/> Todos os direitos reservados</p>
+      </footer>
     </>
   )
 }
