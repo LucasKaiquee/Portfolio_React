edbb62ca62e3882dcb9237fd3a52bd800af5218e
Lucas Kaique <lucaskaique743@gmail.com>
2023-07-25 17:15:53 -0300

    Feat: validando o fomulário

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,51 @@
+import {useForm} from "react-hook-form"
+
 const Form = () => {
+    const { register, handleSubmit, formState: {errors} } = useForm()
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
+    const errorValidation = "border border-[red]"
+
     return (
-        <form action="" className="flex flex-col gap-2 w-[250px] mt-2">
-            <input type="text" placeholder="Nome" className="bg-[#1E1E1E] rounded-[5px] p-2"/>
-            <input type="mail" placeholder="Email" className="bg-[#1E1E1E] rounded-[5px] p-2"/>
-            <textarea name="" id="" cols="30" rows="10" placeholder="Digite sua menssagem..." className="bg-[#1E1E1E] rounded-[5px] h-[150px] p-2"></textarea>
-            <button type="submit" className="bg-[#00B3FF] text-black font-bold rounded-[5px] py-2">Enviar</button>
-        </form>
+        <div className="flex flex-col gap-2 w-[250px] mt-2">
+
+            <input 
+                type="text" 
+                placeholder="Nome" 
+                className={errors?.name && errorValidation}
+                {...register('name', {required: true})}
+                />
+
+            {errors?.name ?.type === 'required' && (
+            <p className="text-[red] font-bold">Insira seu nome</p>
+            )}
+
+            <input 
+                type="mail" 
+                placeholder="Email" 
+                className={errors?.email && errorValidation}
+                {...register('email', {required: true},)}
+                />
+
+            {errors?.email ?.type === 'required' && (
+            <p className="text-[red] font-bold">Insira seu Email</p>
+            )}
+
+            <textarea 
+                name="" 
+                id="" 
+                cols="30" 
+                rows="10" 
+                placeholder="Digite sua menssagem..." 
+                className=""
+                {...register('textArea')}>
+            </textarea>
+
+            <button onClick={() => handleSubmit(onSubmit)()} className="bg-[#00B3FF] text-black font-bold rounded-[5px] py-2">Enviar</button>
+        </div>
     )
 }
 
