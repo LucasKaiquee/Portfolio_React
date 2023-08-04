da621e09ea1df4906ac68ce84e92065baace70b2
Lucas Kaique <lucaskaique743@gmail.com>
2023-08-03 21:09:54 -0300

    Style: definindo os breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,16 @@ export default {
       backgroundImage: {
         'background-main': "url('/src/assets/imgs/background-main.png')",
       },
+      scale: {
+        '125': '1.15',
+      },
+      screens: {
+        '2xl': {'max': '1535px'},
+        'xl': {'max': '1279px'},
+        'lg': {'max': '1023px'},
+        'md': {'max': '767px'},
+        'sm': {'max': '639px'},
+      },
     },
   },
   plugins: [],
