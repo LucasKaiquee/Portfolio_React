09a3b46d652fcb9a9600527329ea0d753c3cf698
LucasKaiquee <lucaskaique743@gmail.com>
2023-09-01 21:40:16 -0300

    Feat: Adicionando um alerta ao enviar o formulario

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,32 @@
 import axios from "axios";
 import {useForm} from "react-hook-form"
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 const Form = () => {
-    const { register, handleSubmit, formState: {errors} } = useForm()
+    const { register, reset, handleSubmit, formState: {errors} } = useForm()
+    const MySwal = withReactContent(Swal)
 
     const onSubmit = async (data) => {
         try {
-          const response = await axios.post('https://formsubmit.co/ajax/lucaskaique743@gmail.com', data);
-          console.log('Resposta da API:', response.data);
+            const response = await axios.post('https://formsubmit.co/ajax/lucaskaique743@gmail.com', data);
+            const status = response.data
+            console.log('Resposta da API:', status);
+            if(status.success === "false"){
+                MySwal.fire({
+                    title: <strong>Obrigado !</strong>,
+                    html: <i>Recebi sua menssagem ...</i>,
+                    icon: 'success'
+                }); 
+                // reset()
+            } else {
+                MySwal.fire({
+                    title: <strong>Erro</strong>,
+                    html: <i>Vou resolver o mais rápido possível ...</i>,
+                    icon: 'error'
+            })}
         } catch (error) {
-          console.error('Erro ao enviar o formulário:', error);
+            console.error('Erro ao enviar o formulário:', error);
         }
       };
 
@@ -51,7 +68,7 @@ const Form = () => {
                 {...register('textArea')}>
             </textarea>
 
-            <button onClick={() => handleSubmit(onSubmit)()} className="bg-[#00B3FF] text-black font-bold rounded-[5px] py-2 hover:scale-125 ease-out duration-300">Enviar</button>
+            <button onClick={() => handleSubmit(onSubmit)()} className="bg-[#00B3FF] text-black font-bold rounded-[5px] py-2 hover:scale-125 ease-out duration-300" id="button-form">Enviar</button>
         </div>
     )
 }
